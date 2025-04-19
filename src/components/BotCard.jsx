d5ef9ee5562d7da229be06330b24eee605fc9303
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-19 12:25:25 +0300

    commit

--- src/components/BotCard.jsx
+++ src/components/BotCard.jsx
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from "react";
+import BotCard from "./BotCard";
+
+function BotCollection({ bots, handleAddToArmy }) {
+  return (
+    <div className="container">
+      <h2>Available Bots</h2>
+      <div className="d-flex flex-wrap">
+        {bots.map((bot) => (
+          <BotCard key={bot.id} bot={bot} handleClick={handleAddToArmy} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BotCollection;
